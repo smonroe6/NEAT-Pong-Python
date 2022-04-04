6bc98447341998f88ef1fccdea94a128e314b16f
Stephen Monroe <smonroe6@vols.utk.edu>
2022-04-04 18:48:31 -0400

    Create class and method

--- Pong_AI.py
+++ Pong_AI.py
@@ -1,22 +1,33 @@
 import pygame
 from pong import Game
 
-width, height = 700, 500
-window = pygame.display.set_mode((width, height))
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
 
-game = Game(window, width, height)
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                game.move_paddle(Left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(Left=True, up=False)
 
-    game.loop()
-    game.draw()
-    pygame.display.update()
+            game_info = game.loop()
+            print(game_info.left_score, game_info.right_score)
+            game.draw()
+            pygame.display.update()
 
-pygame.quit()
+        pygame.quit()
+
